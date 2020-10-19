c6222df87c38753c3411b610895498f005aede75
RemApple <32689703+NganDii@users.noreply.github.com>
2020-10-19 12:40:49 +0530

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,7 +1,4 @@
 bin=/data/data/com.termux/files/usr/bin
-cd ~/
-cd gaming
-mv -f file $bin/gaming && chmod +x $bin/gaming
-test -f $bin/gaming && echo "Successful" 
-echo "type "gaming" to run"
-cd ~/
+loc=$PWD
+cat $loc/file > $bin/gaming
+chmod +x $bin/gaming
