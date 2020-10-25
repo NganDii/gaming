cda69e0eeb2f301884822fc298a8479fea58b2e2
RemApple <32689703+NganDii@users.noreply.github.com>
2020-10-25 19:28:51 +0530

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -2,5 +2,7 @@ bin=/data/data/com.termux/files/usr/bin
 loc=$HOME/gaming
 cat $loc/file > $bin/gaming
 chmod +x $bin/gaming
+echo "clear; gaming; " > $bin/gamingup.sh
+chmod +x $bin/gamingup.sh
 clear
 echo -e "\e[1;92mType "gaming" and hit Enter to RUN\e[0m"
